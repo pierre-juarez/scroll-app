5e00311c06d4faf506b045fa4f4798f0baf34ca0
pierre-juarez <76144339+pierre-juarez@users.noreply.github.com>
2024-11-27 23:58:12 -0500

    [feat] - Initial configuration

---
+++ Scrolls-App/Views/MainVC.swift
@@ -0,0 +1,38 @@
+//
+//  MainVC.swift
+//  Scrolls-App
+//
+//  Created by Jhonatan Chavez on 27/11/24.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    init(){
+        super.init(nibName: "MainVC", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Scrollviews"
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
